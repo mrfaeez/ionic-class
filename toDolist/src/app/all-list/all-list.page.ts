89fc4a4bbc89d8d298fac6fff88acce28ce14a76
mrfaeez <faexd99@gmail.com>
2020-10-11 16:42:57 +0800

    IDK

--- toDolist/src/app/all-list/all-list.page.ts
+++ toDolist/src/app/all-list/all-list.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+// import '@capacitor-community/http';
+import { Plugins } from '@capacitor/core';
 
 @Component({
   selector: 'app-all-list',
@@ -10,6 +12,21 @@ export class AllListPage implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  async getData(){
+    const {Http}=Plugins;
+
+    await Http.request({
+      method: 'GET',
+      url: ''
+    })
+    .then(res=>{
+      console.log(res)
+    })
+    .catch(err=>{
+      console.log(err);
+    })
   }
 
 }
